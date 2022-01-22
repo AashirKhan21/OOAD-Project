cb6ac7d158c895fb17ea68a79fe0be5854dcef8d
Muhammad Aashir Khan <55949359+AashirKhan21@users.noreply.github.com>
2022-01-23 02:36:13 +0500

    Update tbl_BookFine.cs

--- Kutabkhana_DBLayer/tbl_BookFine.cs
+++ Kutabkhana_DBLayer/tbl_BookFine.cs
@@ -11,13 +11,19 @@ namespace Kutabkhana_DBLayer
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class tbl_BookFine
     {
         public int BookFineID { get; set; }
+        [Required(ErrorMessage = "Please Select Employee!")]
         public int EmployeeID { get; set; }
+        [Required(ErrorMessage = "Please Select Book!")]
         public int BookID { get; set; }
+        [Required(ErrorMessage = "Please Select User!")]
         public int UserID { get; set; }
+
+        [DataType(DataType.Date)]
         public System.DateTime FineDate { get; set; }
         public double FineAmount { get; set; }
         public Nullable<double> ReceiveAmount { get; set; }
