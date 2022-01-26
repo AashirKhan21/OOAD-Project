c62cec641e3944812456a73dde8da56beb83555f
Muhammad Aashir Khan <55949359+AashirKhan21@users.noreply.github.com>
2022-01-27 03:00:29 +0500

    Added Login System

--- Kutabkhana/Controllers/HomeController.cs
+++ Kutabkhana/Controllers/HomeController.cs
@@ -15,21 +15,69 @@ namespace Kutabkhana.Controllers
             return View();
         }
 
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+
         [HttpPost]
-        public ActionResult LoginUser(string email, string password)
+        public ActionResult LoginUser(tbl_Users tbl_Users) 
         {
             try
             {
-                if (email != null && password != null)
-                {
-                    var finduser = db
-                }
+                    var finduser = db.tbl_Users.Where(u => u.Username == tbl_Users.Username && u.Password == tbl_Users.Password && u.IsActive == true).FirstOrDefault();
+                    if (finduser != null)
+                    {
+                        Session["UserID"] = finduser.UserID;
+                        Session["UserTypeID"] = finduser.UserTypeID;
+                        Session["Username"] = finduser.Username;
+                        Session["Password"] = finduser.Password;
+                        Session["EmployeeID"] = finduser.EmployeeID;
+                        //UserTypeID       UserTypeName
+                        //1                 Admin
+                        //2                 Operator
+                        //3                 Teacher
+                        //4                 Student
 
+                        if (finduser.UserTypeID == 2)
+                        {
+                            return RedirectToAction("About", "Home");
+                        }
+                        else if (finduser.UserTypeID == 3)
+                        {
+                            return RedirectToAction("About", "Home");
+                        }
+                        else if (finduser.UserTypeID == 4)
+                        {
+                            return RedirectToAction("About", "Home");
+                        }
+                        else if (finduser.UserTypeID == 1)
+                        {
+                            return RedirectToAction("About", "Home");
+                        }
+                    }
+                    else
+                    {
+                        Session["UserID"] = finduser.UserID;
+                        Session["UserTypeID"] = finduser.UserTypeID;
+                        Session["Username"] = finduser.Username;
+                        Session["Password"] = finduser.Password;
+                        Session["EmployeeID"] = finduser.EmployeeID;
+                        ViewBag.Message = "Username and Password is Incorrect!!";
+                        return RedirectToAction("Login", "Index");
+                    }
             }
             catch (Exception ex)
             {
-
+                Session["UserID"] = string.Empty;
+                Session["UserTypeID"] = string.Empty;
+                Session["Username"] = string.Empty;
+                Session["Password"] = string.Empty;
+                Session["EmployeeID"] = string.Empty;
+                ViewBag.Message = ex.Message;
             }
+
+            return View("Login");
         }
         public ActionResult About()
         {
@@ -43,6 +91,11 @@ namespace Kutabkhana.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Logout()
+        {
+            return View("Login");
         }
     }
 }
