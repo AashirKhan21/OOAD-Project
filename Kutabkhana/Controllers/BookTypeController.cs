5a7fa1f42451f10c64415a44bd7e3516c0624fbd
Muhammad Aashir Khan <55949359+AashirKhan21@users.noreply.github.com>
2022-01-29 21:39:37 +0500

    Added Book Type Controller

---
+++ Kutabkhana/Controllers/BookTypeController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Kutabkhana_DBLayer;
+
+namespace Kutabkhana.Controllers
+{
+    public class BookTypeController : Controller
+    {
+        private KutabkhanaDBEntities db = new KutabkhanaDBEntities();
+
+        // GET: BookType
+        public ActionResult Index()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserID"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            return View(db.tbl_BookType.ToList());
+        }
+
+        // GET: BookType/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_BookType tbl_BookType = db.tbl_BookType.Find(id);
+            if (tbl_BookType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_BookType);
+        }
+
+        // GET: BookType/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BookType/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BookTypeID,Name")] tbl_BookType tbl_BookType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbl_BookType.Add(tbl_BookType);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tbl_BookType);
+        }
+
+        // GET: BookType/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_BookType tbl_BookType = db.tbl_BookType.Find(id);
+            if (tbl_BookType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_BookType);
+        }
+
+        // POST: BookType/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BookTypeID,Name")] tbl_BookType tbl_BookType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbl_BookType).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tbl_BookType);
+        }
+
+        // GET: BookType/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_BookType tbl_BookType = db.tbl_BookType.Find(id);
+            if (tbl_BookType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_BookType);
+        }
+
+        // POST: BookType/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbl_BookType tbl_BookType = db.tbl_BookType.Find(id);
+            db.tbl_BookType.Remove(tbl_BookType);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
